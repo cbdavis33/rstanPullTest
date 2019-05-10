3899c54b6230a9ced2ffcaceb10c7c52132c9e89
Casey Davis <cbdavis33@gmail.com>
2019-05-10 11:32:22 -0700

    Initial commit. This is a template for any new packages I write that involve compiled Stan code

---
+++ R/rstanSkeletonDefault-package.R
@@ -0,0 +1,17 @@
+#' The 'rstanSkeletonDefault' package.
+#' 
+#' @description A DESCRIPTION OF THE PACKAGE
+#' 
+#' @docType package
+#' @name rstanSkeletonDefault-package
+#' @aliases rstanSkeletonDefault
+#' @useDynLib rstanSkeletonDefault, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#' 
+NULL
