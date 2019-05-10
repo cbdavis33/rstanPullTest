a99dfc199c53631a53837a962c593946f3071a0d
Casey Davis <cbdavis33@gmail.com>
2019-05-10 12:32:01 -0700

    Checking if this works

--- R/rstanSkeletonDefault-package.R
+++ R/rstanSkeletonDefault-package.R
@@ -1,17 +1,17 @@
-#' The 'rstanSkeletonDefault' package.
-#' 
+#' The 'rstanPullTest' package.
+#'
 #' @description A DESCRIPTION OF THE PACKAGE
-#' 
+#'
 #' @docType package
-#' @name rstanSkeletonDefault-package
-#' @aliases rstanSkeletonDefault
-#' @useDynLib rstanSkeletonDefault, .registration = TRUE
+#' @name rstanPullTest-package
+#' @aliases rstanPullTest
+#' @useDynLib rstanPullTest, .registration = TRUE
 #' @import methods
 #' @import Rcpp
 #' @import rstantools
 #' @importFrom rstan sampling
-#' 
-#' @references 
+#'
+#' @references
 #' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
-#' 
+#'
 NULL
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# rstanSkelDefault
-rstanSkelDefault
+# rstanPullTest
+rstanPullTest
 
-The function rstantools::rstan_package_skeleton() isn't working right. It calls usethis::create_package(), which in turn tells me my project is nested in another package. Continuing to create the package from there creates the package fine, but then there are thousands of irrelevant documents in the Git pane. Instead of messing with it more, I've just created this package and will pull it when I want to write a package involving compiled Stan code.
+This is a test to see if pulling from rstanSkelDefault on Github will work.
